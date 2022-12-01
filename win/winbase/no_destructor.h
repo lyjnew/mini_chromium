047b805119c4ba90dbaf5cdd1f05b2d00ba26e32
morecod <lq.yhbnb@gmail.com>
2022-12-01 19:09:13 +0800

    Update no_destructor.h

--- win/winbase/no_destructor.h
+++ win/winbase/no_destructor.h
@@ -38,7 +38,7 @@ namespace winbase {
 // indirection and a malloc. Also note that since C++11 static local variable
 // initialization is thread-safe and so is this pattern. Code should prefer to
 // use NoDestructor<T> over:
-// - The CR_DEFINE_STATIC_LOCAL() helper macro.
+// - The WINBASE_DEFINE_STATIC_LOCAL() helper macro.
 // - A function scoped static T* or T& that is dynamically initialized.
 // - A global winbase::LazyInstance<T>.
 //
